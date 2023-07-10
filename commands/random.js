18993b34587bbb9977c7284bf089a597e7bf881f
Kanyarat <pigsweet1412@gmail.com>
2023-07-10 22:28:04 +0700

    Update random number

--- commands/random.js
+++ commands/random.js
@@ -3,9 +3,31 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('random')
-		.setDescription('random number'),
+		.setDescription('random number')
+		.addStringOption((option) =>
+		option
+		.setName("from")
+		.setDescription("The number you want to start in random.")
+		.setRequired(true))
+		.addStringOption((option) =>
+		option
+		.setName("to")
+		.setDescription("The last number you want in random.")
+		.setRequired(true)),
 	async execute(interaction) {
-        const number = ["1","2","3","4","5","6","7","8","9","10"]
-		await interaction.reply(number[Math.floor(Math.random()*number.length)]);
+		const startNumber = parseInt(interaction.options.getString("from"));
+  		const endNumber = parseInt(interaction.options.getString("to"));
+		const numberLength = endNumber - startNumber + 1;
+		const numbers = [];
+
+		for(i = startNumber; i <= endNumber; i++){
+			numbers.push(i);
+		}
+
+		const randomIndex = Math.floor(Math.random() * numberLength);
+		const randomNumber = numbers[randomIndex];
+
+		// await interaction.reply(randomNumber);
+		await interaction.reply({ content: String(randomNumber), ephemeral: true });
 	},
 };
